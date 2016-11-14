949b5321841a7f544d5d692933f74315c72a8b8e
Brandon Hubbard <bkhubbard@gmail.com>
2016-11-14 11:15:50 -0800

    started lib

---
+++ wp-google-geocoder-api.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * WP Google GeoCoder API (https://developers.google.com/maps/web/)
+ *
+ * @package WP-GoogleGeoCoder-API
+ */
+
+/*
+* Plugin Name: WP GoogleGeoCoder API
+* Plugin URI: https://github.com/wp-api-libraries/wp-google-geocoder-api
+* Description: Perform API requests toGoogleGeoCoder in WordPress.
+* Author: WP API Libraries
+* Version: 1.0.0
+* Author URI: https://wp-api-libraries.com
+* GitHub Plugin URI: https://github.com/wp-api-libraries/wp-google-geocoder-api
+* GitHub Branch: master
+*/
+
+/* Exit if accessed directly */
+if ( ! defined( 'ABSPATH' ) ) { exit; }
+
+if ( ! class_exists( 'GoogleMapsGeocoderAPI' ) ) {
+
+	/**
+	 * GoogleMapsGeocoderAPI class.
+	 */
+	class GoogleMapsGeocoderAPI {
+	}
+}
